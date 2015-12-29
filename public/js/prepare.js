ee52598484a785890fb31167b1103320fab4a6d2
Steve Zelaznik <zelaznik@yahoo.com>
2015-12-28 20:01:12 -0800

    Changed file structure to fit rack expectations.

--- public/js/prepare.js
+++ public/js/prepare.js
@@ -5,15 +5,15 @@ var window = window || this;
 
   var prepare = window.prepare = function prepare(options) {
     var imgDigits = window.imgDigits = new Image();
-    imgDigits.src = 'assets/digits.png';
+    imgDigits.src = 'images/digits.png';
     imgDigits.addEventListener('load', function () {
 
       var imgTiles = window.imgTiles = new Image();
-      imgTiles.src = 'assets/tiles.png';
+      imgTiles.src = 'images/tiles.png';
       imgTiles.addEventListener('load', function() {
 
         var imgFaces = window.imgFaces = new Image();
-        imgFaces.src = 'assets/faces_glyph.jpg';
+        imgFaces.src = 'images/faces_glyph.jpg';
         imgFaces.addEventListener('load', function() {
 
           var view = window.view = new View({
