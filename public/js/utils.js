c18ddf4d870df8cd30f83d8e3b740f95384d18e5
Steve Zelaznik <zelaznik@yahoo.com>
2015-12-29 14:58:48 -0800

    added transitions on success and failure.

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,6 +3,17 @@ var window = window || this;
 (function() {
   'use strict';
 
+  window.blackAndWhite = function(color) {
+    var pure, red, green, blue, grey, asHex;
+    pure = color.replace('#', '');
+    red = parseInt(pure.slice(0, 2), 16);
+    green = parseInt(pure.slice(2, 4), 16);
+    blue = parseInt(pure.slice(4, 6), 16);
+    grey = 0.3 * red  + 0.59 * green + 0.11 * blue;
+    asHex = (Math.round(grey)).toString(16).toUpperCase();
+    return '#' + asHex + asHex + asHex;
+  };
+
   Function.prototype.inherits = function(Parent, options) {
     var Child = this;
     function Surrogate() {}
