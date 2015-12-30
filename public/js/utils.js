0c66e601c2c1f433d6d3ddc9773b0051a266ea65
Steve Zelaznik <zelaznik@yahoo.com>
2015-12-29 16:55:14 -0800

    The menubar now turns green on success.

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,15 +3,28 @@ var window = window || this;
 (function() {
   'use strict';
 
-  window.blackAndWhite = function(color) {
+  window.makeGrey = function(color) {
     var pure, red, green, blue, grey, asHex;
     pure = color.replace('#', '');
     red = parseInt(pure.slice(0, 2), 16);
     green = parseInt(pure.slice(2, 4), 16);
     blue = parseInt(pure.slice(4, 6), 16);
     grey = 0.3 * red  + 0.59 * green + 0.11 * blue;
+    return grey;
+  };
+
+  window.blackAndWhite = function(color) {
+    var asGrey, asHex;
+    asGrey = makeGrey(color);
     asHex = (Math.round(grey)).toString(16).toUpperCase();
     return '#' + asHex + asHex + asHex;
+  };
+
+  window.hue = function(origColor, weights) {
+    var r = weights.r || weights.red;
+    var g = weights.r || weights.green;
+    var b = weights.b || weights.blue;
+
   };
 
   Function.prototype.inherits = function(Parent, options) {
