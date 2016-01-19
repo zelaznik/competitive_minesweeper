3cd3a397ba58ee65c495850be1892b101ffd6f49
Steve Zelaznik <zelaznik@yahoo.com>
2016-01-19 11:07:46 -0800

    Cleaned up some no-longer-needed code.

--- public/js/prepare.js
+++ public/js/prepare.js
@@ -1,52 +1,6 @@
 (function() {
   'use strict';
 
-  var mineSweeperBoard = window.mineSweeperBoard = function mineSweeperBoard(options) {
-    /* The HTML should look like this:
-      <div id="main" class="game border-convex">
-        <div id="aux" class="aux border-concave">
-          <canvas class='aux-item score-canvas' id="main-score-canvas"></canvas>
-          <canvas class="aux-item faces" id="main-faces-button"></canvas>
-          <canvas class='aux-item timer-canvas' id="main-timer-canvas"></canvas>
-        </div>
-        <canvas class="board border-concave" id="main-board-canvas"></canvas>
-      </div> <!-- End main -->
-    */
-
-    throw "NotImplemented";
-
-    var document = document || options.document;
-    var height = options.height || '256px';
-    var width = options.width || '480px';
-    var category = options.category;
-
-    var $div = $('<div id="' + category + '" class="game border-convex"></div>');
-    $div.attr('style', 'width: ' + width + ';');
-
-      var $aux = $('<div class="aux border-concave"></div>');
-      $div.append($aux);
-
-        var $score = $('<canvas class="aux-item score-canvas"></canvas>');
-        $aux.append($score);
-
-        var $faces = $('<canvas class="aux-item faces"></canvas>');
-        $faces.attr('id', category + '-faces-button');
-        $aux.append($faces);
-
-        var $timer = $('<canvas class="aux-item timer-canvas"></canvas>');
-        $timer.attr('id', category + '-timer-canvas');
-        $aux.append($timer);
-
-      var $board = $('<canvas class="board border-concave"></canvas>');
-      $board.attr('id', category + '-board-canvas');
-      $div.append($board);
-
-      return {
-        main: $div, canvas: $board, resetButton: $faces,
-        aux: $aux, timer_canvas: $timer, score_canvas: $score
-      };
-  };
-
   var respondToButtons = window.respondToButtons = function(options) {
     var choices = document.getElementsByClassName('level-choice');
     var views = options.views;
