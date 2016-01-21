4b0aff7a4b42258e26b493228b6c7306a8e370c7
Steve Zelaznik <zelaznik@yahoo.com>
2016-01-21 00:07:18 -0800

    Animation is jerky but still pauses when game over.

--- public/js/statusbar.js
+++ public/js/statusbar.js
@@ -1,14 +1,3 @@
-$(function() {
-  $(".meter > span").each(function() {
-    $(this)
-      .data("origWidth", $(this).width())
-      .width(0)
-      .animate({
-        width: $(this).data("origWidth")
-      }, 1200);
-  });
-});
-
 (function() {
   'use strict';
 
